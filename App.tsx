2d951682758995d2cb1cfa33d7f724ffe660a506
Wilson Franca <wilson.franca.92@gmail.com>
2024-02-02 08:55:03 -0800

    feat(styles): added stylesheet to App

--- App.tsx
+++ App.tsx
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.eventName}>
+        Event Name
+      </Text>
+      <Text style={styles.eventDate}>
+        Friday, November 4th, 2022
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#131016',
+    padding: 24
   },
-});
+  eventName: {
+    color: '#FDFCFE',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48
+  },
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16
+  }
+})
